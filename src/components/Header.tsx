39f74ebee6ae6379259e96b39cd12c6a80f62cb8
João Bispo <joaobispo2077@gmail.com>
2021-09-07 14:43:22 -0300

    feat: add notification icons and avatar info

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -47,6 +51,34 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" color="gray.400" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>João Bispo</Text>
+            <Text color="gray.300" fontSize="small">
+              joaobispo2077@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="João Bispo"
+            src="https://github.com/joaobispo2077.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
