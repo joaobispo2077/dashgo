ea6c5dcfdcd9a0fccaf8bfae854445b5e1e181cf
João Bispo <joaobispo2077@gmail.com>
2021-09-08 10:03:01 -0300

    feat: handle login form with react hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (form) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(form);
+    console.log('Entering...');
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,6 +27,7 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input
@@ -19,16 +35,24 @@ export default function Home() {
             label="E-mail"
             type="email"
             placeholder="Exemplo: example@gmail.com"
+            {...register('email')}
           />
           <Input
             name="password"
             label="Senha"
             type="password"
             placeholder="Exemplo: **************"
+            {...register('password')}
           />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
