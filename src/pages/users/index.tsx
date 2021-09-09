98a2f5f53dd92440b52999c1b832040736819801
João Bispo <joaobispo2077@gmail.com>
2021-09-09 12:39:34 -0300

    feat: add dynamic user list with stale while revalidate from react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -27,7 +27,18 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    }));
+
+    return users;
   });
 
   const isDesktopScreen = useBreakpointValue({
@@ -81,33 +92,35 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td px={['4', '4', '6']}>
-                      <Box>
-                        <Text fontWeight="bold">João Bispo</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          joaobispo2077@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isDesktopScreen && <Td>04 de Abril, 2021</Td>}
-                    {isDesktopScreen && (
-                      <Td>
-                        <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} />}
-                        >
-                          Editar
-                        </Button>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
                       </Td>
-                    )}
-                  </Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isDesktopScreen && <Td>{user.createdAt}</Td>}
+                      {isDesktopScreen && (
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
