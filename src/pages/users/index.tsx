9afcc08aa743a485d1bba2768015e9485448511d
João Bispo <joaobispo2077@gmail.com>
2021-09-09 12:25:38 -0300

    feat: setup react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
 import {
   useBreakpointValue,
   Box,
@@ -15,22 +14,25 @@ import {
   Checkbox,
   Tbody,
   Text,
+  Spinner,
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('@users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    return data;
+  });
+
   const isDesktopScreen = useBreakpointValue({
     base: false,
     lg: true,
-  });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((r) => r.json())
-      .then(console.log);
   });
 
   return (
@@ -57,49 +59,61 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isDesktopScreen && <Th>Data de cadastro</Th>}
-                {isDesktopScreen && <Th width="8"></Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td px={['4', '4', '6']}>
-                  <Box>
-                    <Text fontWeight="bold">João Bispo</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joaobispo2077@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isDesktopScreen && <Td>04 de Abril, 2021</Td>}
-                {isDesktopScreen && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isDesktopScreen && <Th>Data de cadastro</Th>}
+                    {isDesktopScreen && <Th width="8"></Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td px={['4', '4', '6']}>
+                      <Box>
+                        <Text fontWeight="bold">João Bispo</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          joaobispo2077@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isDesktopScreen && <Td>04 de Abril, 2021</Td>}
+                    {isDesktopScreen && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
