ea6f0f9fbed8b383543e599457768385a92502a1
João Bispo <joaobispo2077@gmail.com>
2021-09-10 10:52:23 -0300

    refactor: extract get users from useUsers hoo

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,28 +1,33 @@
 import { useQuery } from 'react-query';
 import { api } from '../services/api';
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
+export async function getUsers(): Promise<User[]> {
+  const response = await api.get<{ users: User[] }>('/users');
+  const { data } = response;
+
+  const users = data.users.map((user) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+  }));
+
+  return users;
+}
+
 export const useUsers = () => {
-  return useQuery(
-    'dashgo@users',
-    async () => {
-      const response = await api.get('/users');
-      const { data } = response;
-
-      const users = data.users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      }));
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    },
-  );
+  return useQuery('dashgo@users', getUsers, {
+    staleTime: 1000 * 5,
+  });
 };
